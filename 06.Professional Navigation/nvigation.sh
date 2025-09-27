5b993f8f0aeb411e26c6f37eed52921cd4ee559a
samiulAsumel <sa.sumel91@gmail.com>
2025-09-27 21:05:10 +0600

    navigation script upload

---
+++ 06.Professional Navigation/nvigation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ============================================================
+# Enterprise File System Navigation Mastery
+# Author: Samiul A Sumel
+# ============================================================
+
+echo "Welcome to the Enterprise File System Navigation Mastery Script!"
+echo "This script will guide you through essential file system navigation commands."
+echo "Let's get started!"
+
+# Created professional Practice Environment
+sudo mkdir -p /opt/company/{web,db,logs,backup,configs,scripts,temp}
+sudo mkdir -p /opt/company/web/{html,cgi-bin,ssl}
+sudo mkdir -p /opt/company/db/{mysql,postgresql,backups}
+sudo mkdir -p /opt/company/logs/{nginx,apache,mysql,application}
+
+#Set corporate permissions
+sudo chmod alma:alma /opt/company
+sudo chmod 755 /opt/company
+sudo chmod 750 /opt/company/ssl
+
+echo "Enterprise directories created under /opt/company with appropriate permissions."
